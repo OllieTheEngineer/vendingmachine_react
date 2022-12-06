c704ded8cf88428b4f2ddf34f96706f42f1ed9b9
ollie <olivinanzali07@gmail.com>
2022-12-05 18:41:31 -0600

    added changes and added styling to navbar

--- src/NavLink.js
+++ src/NavLink.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import './NavLink.css';
 
 const NavBar = () => {
     return (
         <nav className="NavBar">
-            <NavLink exact to="/">
+            <NavLink exact to="/" className="link">
                 VendingMachine
             </NavLink>
             <NavLink exact to="/Popcorn">
